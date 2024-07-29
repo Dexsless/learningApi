4a9b0102799956db08269c71a80c6ec70ca3b445
Dexsless <dexsless@gmail.com>
2024-07-29 17:00:54 +0700

    1.0.0

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -75,41 +75,41 @@ class KategoriController extends Controller
     }
 }
 
-    public function update(Request $request, $id)
-{
-    $validator = Validator::make($request->all(), [
-        'nama_kategori' => 'required|unique:kategoris,nama_kategori,' . $id
-    ]);
+//     public function update(Request $request, $id)
+// {
+//     $validator = Validator::make($request->all(), [
+//         'nama_kategori' => 'required|unique:kategoris,nama_kategori,' . $id
+//     ]);
 
-    if ($validator->fails()) {
-        $res = [
-            'success' => false,
-            'message' => 'Validasi Gagal',
-            'errors' => $validator->errors()
-        ];
-        return response()->json($res, 422);
-    }
+//     if ($validator->fails()) {
+//         $res = [
+//             'success' => false,
+//             'message' => 'Validasi Gagal',
+//             'errors' => $validator->errors()
+//         ];
+//         return response()->json($res, 422);
+//     }
 
-    try {
-        $kategori = Kategori::findOrFail($id);
-        $kategori->nama_kategori = $request->nama_kategori;
-        $kategori->slug = Str::slug($request->nama_kategori);
-        $kategori->save();
+//     try {
+//         $kategori = Kategori::findOrFail($id);
+//         $kategori->nama_kategori = $request->nama_kategori;
+//         $kategori->slug = Str::slug($request->nama_kategori);
+//         $kategori->save();
 
-        $res = [
-            'success' => true,
-            'message' => 'Data Kategori Berhasil Diubah',
-            'data' => $kategori
-        ];
-        return response()->json($res, 200);
-    } catch (\Exception $e) {
-        $res = [
-            'success' => false,
-            'message' => 'Terjadi kesalahan',
-            'errors' => $e->getMessage()
-        ];
-        return response()->json($res, 500);
-    }
-}
+//         $res = [
+//             'success' => true,
+//             'message' => 'Data Kategori Berhasil Diubah',
+//             'data' => $kategori
+//         ];
+//         return response()->json($res, 200);
+//     } catch (\Exception $e) {
+//         $res = [
+//             'success' => false,
+//             'message' => 'Terjadi kesalahan',
+//             'errors' => $e->getMessage()
+//         ];
+//         return response()->json($res, 500);
+//     }
+// }
 
 }
