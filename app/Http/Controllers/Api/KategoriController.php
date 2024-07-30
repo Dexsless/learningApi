c3115d1e09fbbad08a8b59646474d078f7003ab3
Dexsless <dexsless@gmail.com>
2024-07-30 13:54:15 +0700

    1.0.0

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -56,60 +56,78 @@ class KategoriController extends Controller
         }
     }
     public function show($id)
-{
-    try {
-        $kategori = Kategori::findOrFail($id);
-        $res = [
-            'success' => true,
-            'message' => 'Data Kategori Ditemukan',
-            'data' => $kategori
-        ];
-        return response()->json($res, 200);
-    } catch (\Exception $e) {
-        $res = [
-            'success' => false,
-            'message' => 'Data Kategori Tidak Ditemukan',
-            'errors' => $e->getMessage()
-        ];
-        return response()->json($res, 404);
+    {
+        try {
+            $kategori = Kategori::findOrFail($id);
+            $res = [
+                'success' => true,
+                'message' => 'Data Kategori Ditemukan',
+                'data' => $kategori
+            ];
+            return response()->json($res, 200);
+        } catch (\Exception $e) {
+            $res = [
+                'success' => false,
+                'message' => 'Data Kategori Tidak Ditemukan',
+                'errors' => $e->getMessage()
+            ];
+            return response()->json($res, 404);
+        }
     }
-}
 
-//     public function update(Request $request, $id)
-// {
-//     $validator = Validator::make($request->all(), [
-//         'nama_kategori' => 'required|unique:kategoris,nama_kategori,' . $id
-//     ]);
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'nama_kategori' => 'required'
+        ]);
+        if($validator->fails()){
+            $res = [
+                'success' => false,
+                'message' => 'Validasi Gagal',
+                'errors' => $validator->errors()
+            ];
+            return response()->json($res, 422);
+        }
 
-//     if ($validator->fails()) {
-//         $res = [
-//             'success' => false,
-//             'message' => 'Validasi Gagal',
-//             'errors' => $validator->errors()
-//         ];
-//         return response()->json($res, 422);
-//     }
+        try {
+            $kategori = Kategori::findOrFail($id);
+            $kategori->nama_kategori = $request->nama_kategori;
+            $kategori->slug = Str::slug($request->nama_kategori);
+            $kategori->save();
+            $res = [
+                'success' => true,
+                'message' => 'Data Kategori Tersimpan',
+                'data' => $kategori
+            ];
+            return response()->json($res, 200);
+        } catch (\Exception $e) {
+            $res = [
+                'success' => false,
+                'message' => 'Terjadi kesalahan',
+                'errors' => $e->getMessage()
+            ];
+            return response()->json($res, 500);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $kategori = Kategori::findOrFail($id);
+            $kategori->delete();
 
-//     try {
-//         $kategori = Kategori::findOrFail($id);
-//         $kategori->nama_kategori = $request->nama_kategori;
-//         $kategori->slug = Str::slug($request->nama_kategori);
-//         $kategori->save();
-
-//         $res = [
-//             'success' => true,
-//             'message' => 'Data Kategori Berhasil Diubah',
-//             'data' => $kategori
-//         ];
-//         return response()->json($res, 200);
-//     } catch (\Exception $e) {
-//         $res = [
-//             'success' => false,
-//             'message' => 'Terjadi kesalahan',
-//             'errors' => $e->getMessage()
-//         ];
-//         return response()->json($res, 500);
-//     }
-// }
+            $res = [
+                'success' => true,
+                'message' => 'Data Kategori Berhasil Dihapus'
+            ];
+            return response()->json($res, 200);
+        } catch (\Exception $e) {
+            $res = [
+                'success' => false,
+                'message' => 'Terjadi kesalahan',
+                'errors' => $e->getMessage()
+            ];
+            return response()->json($res, 500);
+        }
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,4 @@ Route::get('kategori', [KategoriController::class, 'index']);
 Route::post('kategori/create', [KategoriController::class, 'store']);
 Route::get('kategori/{id}', [KategoriController::class, 'show']);
 Route::put('kategori/{id}', [KategoriController::class, 'update']);
+Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
