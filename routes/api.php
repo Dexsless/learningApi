d735ded649a820358a880b210ef7736ed7f76bbe
Dexsless <dexsless@gmail.com>
2024-07-31 17:03:45 +0700

    1.0.4

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BeritaController;
 use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\TagController;
@@ -7,17 +8,22 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user/profile', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 // Route::get('kategori', [KategoriController::class, 'index']);
 // Route::post('kategori', [KategoriController::class, 'store']);
 // Route::get('kategori/{id}', [KategoriController::class, 'show']);
 // Route::put('kategori/{id}', [KategoriController::class, 'update']);
 // Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
-Route::apiResource('kategori', KategoriController::class);
-Route::apiResource('tag', TagController::class);
-Route::apiResource('user', UserController::class);
-Route::apiResource('berita', BeritaController::class);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('kategori', KategoriController::class);
+    Route::apiResource('tag', TagController::class);
+    Route::apiResource('user', UserController::class);
+    Route::apiResource('berita', BeritaController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
